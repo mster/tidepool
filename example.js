1b2124f50528b45e995a7e7c794b6fd34d62ddcc
mster <michaelsterpka@gmail.com>
2020-03-30 21:29:17 -0700

    note: MORE HAMMERING FIXED IT

--- example.js
+++ example.js
@@ -1,19 +1,30 @@
 "use strict";
 
-const reqi = require("reqi");
 const Tidepool = require("./index.js");
 
 const tide = new Tidepool();
 
 let urchin = tide.spinUp({ keepAlive: true });
-
-function fn() {
-  console.log("hello wurld");
-}
-
-let fn2 = Object.assign({}, { fn });
-console.log(fn2);
+let speedyUrchin = tide.spinUp({ keepAlive: true });
 
 urchin.postMessage({
-  load: "/home/m/code/worker-threads/tidepool/lib/prime.js"
+  load: "./prime.js"
 });
+
+speedyUrchin.postMessage({
+  load: "./prime.js"
+});
+
+urchin.postMessage({
+  exec: 11111111111
+});
+
+speedyUrchin.postMessage({
+  exec: 9999999999
+});
+
+setTimeout(() => {
+  let lateUrchin = tide.spinUp({ keepAlive: true });
+  lateUrchin.postMessage({ load: "./prime.js" });
+  lateUrchin.postMessage({ exec: 42042069 });
+}, 20000);
